8a735ed41639f4aeff6ca4917a31bb2f1f54e780
sudipm83 <sudipm2000@gmail.com>
2023-03-20 17:32:28 +0530

    Initial commit

--- src/main/java/org/example/PubSubClient.java
+++ src/main/java/org/example/PubSubClient.java
@@ -2,42 +2,60 @@ package org.example;
 
 import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.pubsub.v1.Subscriber;
-import com.google.cloud.pubsub.v1.TopicAdminClient;
-import com.google.cloud.pubsub.v1.TopicAdminSettings;
+import com.google.cloud.pubsub.v1.*;
 import com.google.pubsub.v1.ProjectTopicName;
 import com.google.pubsub.v1.ProjectSubscriptionName;
 import com.google.pubsub.v1.PubsubMessage;
 import com.google.pubsub.v1.SubscriptionName;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class PubSubClient {
-    private static final String PROJECT_ID = "your-project-id";
-    private static final String TOPIC_ID = "your-topic-id";
-    private static final String SUBSCRIPTION_ID = "your-subscription-id";
+    private static String PROJECT_ID;
+    private static String TOPIC_ID;
+    private static String SUBSCRIPTION_ID;
 
-    public static void main(String[] args) throws Exception {
+    GoogleCredentials credentials;
+    TopicAdminClient topicAdminClient;
+    Subscriber subscriber;
+    public PubSubClient(String PROJECT_ID, String TOPIC_ID, String SUBSCRIPTION_ID) throws IOException {
         // Create Pub/Sub client object
-        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream("path/to/your/keyfile.json"));
-        TopicAdminClient topicAdminClient = TopicAdminClient.create(TopicAdminSettings.newBuilder().setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build());
-        Subscriber subscriber = Subscriber.newBuilder(ProjectSubscriptionName.of(PROJECT_ID, SUBSCRIPTION_ID), new MessageReceiverImpl()).setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build();
+        this.PROJECT_ID = PROJECT_ID;
+        this.TOPIC_ID = TOPIC_ID;
+        this.SUBSCRIPTION_ID = SUBSCRIPTION_ID;
 
+        try {
+            credentials = GoogleCredentials.fromStream(new FileInputStream("path/to/your/keyfile.json"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+
+        }
+        topicAdminClient = TopicAdminClient.create(TopicAdminSettings.newBuilder().setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build());
+        subscriber = Subscriber.newBuilder(ProjectSubscriptionName.of(PROJECT_ID, SUBSCRIPTION_ID), new MessageReceiverImpl()).setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build();
+
+    }
+
+    public void getTopic() throws Exception {
         // Create a new topic, or get an existing topic by name
         ProjectTopicName topicName = ProjectTopicName.of(PROJECT_ID, TOPIC_ID);
-        if (!topicAdminClient.listTopics("projects/" + PROJECT_ID).iterateAll().contains(topicName)) {
+        if (!isTopicExists()) {
             topicAdminClient.createTopic(topicName);
         }
+    }
 
+    public void getSubscription() throws InterruptedException {
         // Create a new subscription to the topic, or get an existing subscription by name
         SubscriptionName subscriptionName = SubscriptionName.of(PROJECT_ID, SUBSCRIPTION_ID);
         subscriber.startAsync().awaitRunning();
-
         // Receive messages from the subscription
         Thread.sleep(10000);
         subscriber.stopAsync();
         subscriber.awaitTerminated();
     }
+//    public static void main(String[] args) throws Exception {
+//    }
 
     static class MessageReceiverImpl implements MessageReceiver {
         @Override
@@ -46,4 +64,10 @@ public class PubSubClient {
             consumer.ack();
         }
     }
+
+    public boolean isTopicExists() throws Exception {
+        ProjectTopicName topicName = ProjectTopicName.of(PROJECT_ID, TOPIC_ID);
+            return topicAdminClient.getTopic(topicName) != null;
+        }
+
 }
